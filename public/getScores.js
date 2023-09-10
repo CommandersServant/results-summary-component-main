24b035701220678762787aa3257610772ef7548f
rasheed <rasheed.team002@gridirontest.com>
2023-09-10 19:07:59 +0100

    get data from json file

--- public/getScores.js
+++ public/getScores.js
@@ -1,36 +1,35 @@
-const data = 
-`
-[
-    {
-      "category": "Reaction",
-      "score": 80,
-      "icon": "./assets/images/icon-reaction.svg"
-    },
-    {
-      "category": "Memory",
-      "score": 92,
-      "icon": "./assets/images/icon-memory.svg"
-    },
-    {
-      "category": "Verbal",
-      "score": 61,
-      "icon": "./assets/images/icon-verbal.svg"
-    },
-    {
-      "category": "Visual",
-      "score": 72,
-      "icon": "./assets/images/icon-visual.svg"
+const getJson = async () => {
+    try {
+        const response = await fetch('/public/data.json')
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        } else {
+            throw new Error('Request failed!');
+        }
+    } catch (error) {
+        console.log(error)
     }
-]
-`
+}
 
-const dataObj = JSON.parse(data);
+const getScores = async () => {
+    const data = await getJson();
+    const scoreReaction = data[0].score;
+    const scoreMemory = data[1].score;
+    const scoreVerbal = data[2].score;
+    const scoreVisual = data[3].score;
 
-const scoreReaction = dataObj[0].score;
-const scoreMemory = dataObj[1].score;
-const scoreVerbal = dataObj[2].score;
-const scoreVisual = dataObj[3].score;
+    const scoreSum = (parseInt(scoreReaction) + parseInt(scoreMemory) + parseInt(scoreVerbal) + parseInt(scoreVisual));
+    const scoreAverage = Math.round(scoreSum / 4);
+    const percentage = Math.round((65 * scoreAverage) / 76);
 
-const scoreSum = (parseInt(scoreReaction) + parseInt(scoreMemory) + parseInt(scoreVerbal) + parseInt(scoreVisual));
-const scoreAverage = Math.round(scoreSum / 4);
-const percentage = Math.round((65 * scoreAverage) / 76);
+    resultScore.innerHTML = scoreAverage;
+    reactionScore.innerHTML = scoreReaction;
+    memoryScore.innerHTML = scoreMemory;
+    verbalScore.innerHTML = scoreVerbal;
+    visualScore.innerHTML = scoreVisual;
+    percentageAmount.innerHTML = percentage.toString() + "%"
+
+}
+
+getScores()
--- public/main.js
+++ public/main.js
@@ -4,10 +4,3 @@ const memoryScore = document.querySelector('#memory-score');
 const verbalScore = document.querySelector('#verbal-score');
 const visualScore = document.querySelector('#visual-score');
 const percentageAmount = document.querySelector('#percentage');
-
-resultScore.innerHTML = scoreAverage;
-reactionScore.innerHTML = scoreReaction;
-memoryScore.innerHTML = scoreMemory;
-verbalScore.innerHTML = scoreVerbal;
-visualScore.innerHTML = scoreVisual;
-percentageAmount.innerHTML = percentage.toString() + "%"
